442467ba53025a10f64c8dc36ae98d88aae8eed9
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2023-01-27 13:10:54 +0100

    Add docstring & tests, fix topology list copy bug
    Google Docstring style was adopted for the docstrings.
    
    Bug in generate_topology fixed. list.copy() caused the the lists in
    topology['r_min'] to be the same as in topology['eps'].
    Fixed by explicitly initializing the second list too.
    
    Basic linting and code reformating.

--- monte-carlo-sim/monte_carlo_sim/handle_xyz.py
+++ monte-carlo-sim/monte_carlo_sim/handle_xyz.py
@@ -1,3 +1,4 @@
+"""File Operations and Datastructures"""
 from __future__ import annotations
 
 import math
@@ -6,20 +7,37 @@ from pathlib import Path
 import tomli
 from typing import Iterable, NamedTuple, TypeAlias
 
+_base_path = Path(__file__).parent
+
 
 class Particle(NamedTuple):
     """Tuple container to store information about one Particle in the simulation.
 
-    Implements the string magic method to format it to print into xyz-file format.
+    The Particle tuple impolements the string magic method to format the tuple
+    to match the XYZ-file format for Particles.
+    In combination with Frames, this makes writing XYZ-files more intuitiv.
 
-    :param name: str
-        name of the Atom or Atomtype
-    :param x: float
-        x coordinate of particle
-    :param y: float
-        y coordinate of particle
-    :param z: float
-        z coordinate of particle
+    Args:
+        name (str): name of the Atom
+            Name of the Atom may either be the Element Symbol or the Atomtype
+        x (float): X coordinate of Atom in Angstroms
+        y (float): Y coordinate of Atom in Angstroms
+        z (float): Z coordinate of Atom in Angstroms
+
+    Returns:
+        tuple: containing the provided arguments in the order specified above.
+
+    Examples:
+        >>> hydrogen = Particle(name='H', x=0., y=0., z=0.)
+        >>> hydrogen
+        Particle(name='H', x=0.0, y=0.0, z=0.0)
+        >>> str(hydrogen).split()
+        ['H', '0.0', '0.0', '0.0']
+        >>> hydrogen.name
+        'H'
+        >>> _, *coords = hydrogen
+        >>> coords
+        [0.0, 0.0, 0.0]
     """
     name: str
     x: float
@@ -31,16 +49,39 @@ class Particle(NamedTuple):
 
 
 class Frame(NamedTuple):
-    """Tuple container to store information about one Frame in a simulation.
+    """Tuple container to store information about one structure from XYZ-Format.
 
-    Implements the string magic method to format the frame into xyz-file format.
+    A Frame is just one structure / configuration. Multiple of these Frames,
+    where the number and order of Particles stays the same then form a
+    trajectory.
 
-    :param n_particles: int
-        number of particles in the frame
-    :param comment: str
-        comment about that frame / structure. No restrictions here.
-    :param particles: list of Particles
-        particles in this Frame
+    The Frame Tuple implements the string magic method making writing the frame
+    into XYZ-file format more intuitiv.
+
+    Args:
+        n_particles (int): number of particles in the Frame / structure
+        comment (str): comment about that Frame / structure
+        particles (list[Particle]): individual particles in this Frame / structure
+
+    Returns:
+        tuple: containing all information associated with one structure.
+            The order of the tuple elements is as specified above.
+
+    Examples:
+        >>> coords = [Particle('O', 0., 0., 0.),
+        ...           Particle('H', 0.758602, 0., 0.504284),
+        ...           Particle('H', 0.758602, 0., -0.504284),]
+        >>> water = Frame(n_particles=3, comment='Water', particles=coords)
+        >>> water.n_particles
+        3
+        >>> water.comment
+        'Water'
+        >>> len(water.particles)
+        3
+        >>> print(water)
+        3
+        Water
+        ...
     """
     n_particles: int
     comment: str
@@ -50,47 +91,66 @@ class Frame(NamedTuple):
         return '\n'.join([str(self.n_particles), self.comment,
                           *[str(p) for p in self.particles]])
 
-Topology: TypeAlias = dict[str, list[list[float]]]
 
 # Particle = namedtuple('Particle', ['name', 'x', 'y', 'z'])
 # Frame = namedtuple('Frame', ['n_particles', 'comment', 'particles'])
+Topology: TypeAlias = dict[str, list[list[float]]]
+"""dictionary, Topology type that matches to a given list of Particles.
+
+The dictionary maps strings denoting the parameter name to a 2D list.
+Entry $ij$ is the combined parameter from particles $i$ and $j$.
+"""
+
 
 def read_xyz(file: Iterable[str]) -> list[Frame]:
-    """Read XYZ-file or list of strings into Frames.
+    """Read XYZ-file or list of strings into a list of Frames.
 
-    The XYZ-file can contain multiple frames or only one frame. In any case a list of
-    Frames will be returned.
+    The XYZ-file can contain multiple frames or only one frame.
 
-    :param file: Iterable that returns strings
-        Filehandle or list of strings that contain the XYZ-file information.
+    Args:
+        file: contains the XYZ-file information.
 
-    :returns: list of Frames
+    Returns:
+        list[Frames]: all XYZ-file information stored in Frames.
 
-    :raises: TypeError
-        if the first line of a Frame does not contain a number denoting the
-        number of particles in that Frame.
-    :raises: ValueError
-        if the number of particles does not match the number specified in the
-        first line of a Frame.
+    Raises:
+        TypeError: If the first line of a Frame does not contain a number
+            denoting the number of particles in that Frame.
+        ValueError: If the number of particles does not match the number
+            specified in the first line of a Frame.
+
+    Examples:
+        >>> file = ['  3 ', 'Angstrom ', '  O  0.000000  0.000000  0.000000',
+        ...         '  H  0.758602  0.000000  0.504284',
+        ...         '  H  0.758602  0.000000  -0.504284']
+        >>> frames = read_xyz(file)
+        >>> type(frames)
+        <class 'list'>
+        >>> len(frames)
+        1
+        >>> water = frames[0]
+        >>> water.comment
+        'Angstrom'
     """
     file = iter(file)
     trajectory: list[Frame] = []
     while True:
         try:
             header: str = next(file).strip()
-            n_particles: int = int(header)
         except StopIteration:
             return trajectory
+        try:
+            n_particles: int = int(header)
         except ValueError:
-            raise TypeError('Expected a integer denoting the number of particles in that frame, '
-                             f'received {header!r}.')
+            raise TypeError('Expected a integer denoting the number of '
+                            f'particles in that frame, received {header!r}.')
         try:
             comment: str = next(file).strip()
 
             particles: list[Particle] = []
             for _ in range(n_particles):
-                name, x, y, z = next(file).strip().split()
-                x, y, z = float(x), float(y), float(z)
+                name, _x, _y, _z = next(file).strip().split()
+                x, y, z = float(_x), float(_y), float(_z)
 
                 particle = Particle(
                         name=name,
@@ -107,53 +167,88 @@ def read_xyz(file: Iterable[str]) -> list[Frame]:
                 )
             trajectory.append(frame)
         except StopIteration:
-            raise ValueError('File ended early. '
-                             'Number of particles does not match or comment line is missing')
+            raise ValueError('File ended early. Number of particles does not '
+                             'match or comment line is missing')
 
 
-def generate_topology(coordinates: list[Particle], parameters: dict[str, dict[str, float]]) -> Topology:
-    """Generate Topology dictionary that matches to the coordinates of one Frame.
+def generate_topology(coordinates: list[Particle],
+                      parameters: dict[str, dict[str, float]],
+                      ) -> Topology:
+    """Generate a Topology dictionary for a given Particle list.
 
-    The topology is only valid for Frames that have the very same order of Particles as
-    the Particle list the topology has been generated from.
+    The topology is only valid for Frames that have the very same order of
+    Particles as the Particle list the topology has been generated from.
 
-    :param coordinates: list of Particles
-        Particles in one Frame to generate the topology for
-    :param parameters: dictionary
-        This dictionary should contains the Atomname as key and as value a
-        dictionary that maps parameters to value.
+    Args:
+        coordinates: Particles to generate the topology for
+        parameters: nested dictionary containing parameters for atomtypes
+            The mapping schematically is as follows:
+                Atomtype -> parameter_name -> value
 
-    :returns: Topology
+    Returns: Topology
         Dictionary with the parameter as key and as values a symmetric 2D list.
-        The list contains in position i, j the parameter p_ij.
-        For different parameters this can be either a arithmetic or geometric mean.
+        The list contains in position $i$, $j$ the parameter $p_{ij}$.
+        Parameter $p_{ij}$ can be either a arithmetic or geometric mean of
+        $p_i$ and $p_j$.
+
+    Examples:
+        >>> a = Particle('a', 0, 0, 0)
+        >>> b = Particle('b', 0, 0, 0)
+        >>> parameters = {'a': {'eps': 2, 'r_min': 2},
+        ...               'b': {'eps': 50, 'r_min': 4}}
+        >>> top = generate_topology([a, b], parameters)
+        >>> top['eps']
+        [[2.0, 10.0], [10.0, 50.0]]
+        >>> top['r_min']
+        [[2.0, 3.0], [3.0, 4.0]]
+
     """
     t_eps: list[list[float]] = [[0. for _ in coordinates] for _ in coordinates]
-    t_r = t_eps.copy()
+    t_r: list[list[float]] = [[0. for _ in coordinates] for _ in coordinates]
     for i, particle_a in enumerate(coordinates):
-       for j, particle_b in enumerate(coordinates[i:], start=i):
-           # geometric mean for epsilon parameter of lennard jones law
-           t_eps[i][j] = math.sqrt(
-                   parameters[particle_a.name]['eps'] * parameters[particle_b.name]['eps']
-                )
-           t_eps[j][i] = t_eps[i][j]
-           # arithmetic mean for equilibrium distance r_min
-           t_r[i][j] = (parameters[particle_a.name]['r_min'] + parameters[particle_b.name]['r_min']) / 2
-           t_r[j][i] = t_r[i][j]
+        for j, particle_b in enumerate(coordinates[i:], start=i):
+            _a = parameters[particle_a.name]
+            _b = parameters[particle_b.name]
+            # geometric mean for epsilon
+            t_eps[i][j] = math.sqrt(
+                    _a['eps'] * _b['eps']
+                    )
+            t_eps[j][i] = t_eps[i][j]
+            # arithmetic mean for epsilon
+            t_r[i][j] = (_a['r_min'] + _b['r_min']) / 2
+            t_r[j][i] = t_r[i][j]
 
     topology = {
-            'r_min' : t_r,
+            'r_min': t_r,
             'eps': t_eps,
         }
     return topology
 
 
-def load_start_structure(filename: str | PathLike,
-                         parameter_file: str | PathLike = Path(__file__).parent / 'parameters.toml') -> tuple[Frame, Topology]:
-    """Open and read data from input files and generate start frame and correspoding topology."""
-    with open(filename, 'r') as cf:
-        first_frame = read_xyz(cf)[0]
+def load_start_structure(
+        xyz_file: str | PathLike,
+        parameter_file: str | PathLike = _base_path / 'parameters.toml',
+        ) -> tuple[list[Particle], Topology]:
+    """Read XYZ File and generate a start Frame with a correspoding topology.
+
+    Args:
+        xyz_file: file in XYZ format
+            contains coordinate and atomtype information
+        parameter (optional): parameter file in toml format
+            has the general format of
+                [Atomtype]
+                parameter = value
+            if not provided, use the default parameter file `parameters.toml`.
+
+    Returns: list[Particle], Topology
+        list with all particles and a matching topology.
+    """
+    with open(xyz_file, 'r') as coordinate_file:
+        first_frame = read_xyz(coordinate_file)[0]
     with open(parameter_file, 'rb') as pf:
         parameters = tomli.load(pf)
-    topology = generate_topology(coordinates=first_frame.particles, parameters=parameters)
-    return first_frame, topology
+    topology = generate_topology(
+            coordinates=first_frame.particles,
+            parameters=parameters,
+            )
+    return first_frame.particles, topology
